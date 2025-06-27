c3b4e00015ed660fb82cdfb64a526ca84c028ff7
kvenkat9889 <kvenkat9889@gmail.com>
2025-06-27 16:53:56 +0530

    updated

--- init.sql
+++ init.sql
@@ -1,12 +1,12 @@
- CREATE TABLE IF NOT EXISTS asset_requests (
+CREATE TABLE IF NOT EXISTS asset_requests (
                 id SERIAL PRIMARY KEY,
-                employee_id VARCHAR(50) NOT NULL,
-                employee_name VARCHAR(100) NOT NULL,
-                email VARCHAR(50) NOT NULL,
-                request_date DATE NOT NULL,
-                asset_type VARCHAR(50) NOT NULL,
-                asset_name VARCHAR(40) NOT NULL,
-                details TEXT NOT NULL,
-                status VARCHAR(20) DEFAULT 'Pending' CHECK (status IN ('Pending', 'Approved', 'Rejected')),
+                employee_id VARCHAR(7),
+                employee_name VARCHAR(40),
+                email VARCHAR(40),
+                request_date DATE,
+                asset_type VARCHAR(50),
+                asset_name VARCHAR(30),
+                details VARCHAR(150),
+                status VARCHAR(20) DEFAULT 'Pending',
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
